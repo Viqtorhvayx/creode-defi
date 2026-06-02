a76c6b2eafaa37d72b6f3b2afad6ceccf7b2342a
Viqtorhvayx <viqtorhvayx@users.noreply.github.com>
2026-06-03 00:44:33 +0100

    style: revert vault card outline opacity back to 50%

--- frontend/src/components/VaultTab.tsx
+++ frontend/src/components/VaultTab.tsx
@@ -27,7 +27,7 @@ export const VaultTab: React.FC<VaultTabProps> = ({ theme }) => {
         </div>
 
         {/* RIGHT COLUMN: Vault Lock Card (Spans 5 columns) */}
-        <div className="lg:col-span-5 bg-white dark:bg-[#0F141A] border border-[#00A8E8]/30 rounded-[16px] p-8 flex flex-col relative overflow-hidden h-fit">
+        <div className="lg:col-span-5 bg-white dark:bg-[#0F141A] border border-[#00A8E8]/50 rounded-[16px] p-8 flex flex-col relative overflow-hidden h-fit">
           
           {/* Header Row */}
           <div className="flex justify-between items-start mb-8">
