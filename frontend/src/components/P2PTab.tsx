7d3276316cf7ed9b0720126b8ddedd8be101b770
Viqtorhvayx <viqtorhvayx@users.noreply.github.com>
2026-07-03 11:27:19 +0100

    chore: remove black background from empty P2P tab

--- frontend/src/components/P2PTab.tsx
+++ frontend/src/components/P2PTab.tsx
@@ -6,7 +6,7 @@ interface P2PTabProps {
 
 export const P2PTab: React.FC<P2PTabProps> = ({ theme }) => {
   return (
-    <div className="w-full h-full flex flex-col text-white bg-[#06080A] min-h-screen">
+    <div className="w-full h-full flex flex-col text-white">
       {/* Completely empty P2P tab as requested */}
     </div>
   );
